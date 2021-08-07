f1d874a315de9362e4c38e1d11a756dfb3f96b26
Artem Titoulenko <artem.titoulenko@gmail.com>
2021-08-07 02:14:46 -0400

    simplify commands

--- conversions.py
+++ conversions.py
@@ -1,33 +1,32 @@
 import sublime
 import sublime_plugin
+import re
 
 class Base(sublime_plugin.TextCommand):
     def run(self, edit):
         regions = self.view.sel()
         for region in regions:
             text = self.view.substr(region)
-            self.view.replace(edit, region, self.op(text))
+            self.view.replace(edit, region, self.op(self.sanitize(text)))
+
+    def sanitize(self, text):
+        return re.sub(r"([\s\#])", "", text.lower())
     
     def op(self, text):
         return text
 
-class BinToByteCommand(Base):
+class BinToHexCommand(Base):
     def op(self, text):
-        return "#{0:02x}".format(int(text.replace(" ", ""), 2))
+        return "#{0:04x}".format(int(text, 2))
 
-class BinToShortCommand(Base):
+class HexToBinCommand(Base):
     def op(self, text):
-        return "#{0:04x}".format(int(text.replace(" ", ""), 2))
-
-class ByteToBin(Base):
-    def op(self, text):
-        return "{0:08b}".format(int(text.replace("#", ""), 16))
-
-class ShortToBin(Base):
-    def op(self, text):
-        text = text.replace("#", "")
         return " ".join(["{0:08b}".format(int(text[i:i+2], 16)) for i in range(0, len(text), 2)])
 
-class IntToByte(Base):
+class HexToIntCommand(Base):
+    def op(self, text):
+        return "".join([str(int(text[i:i+2], 16)) for i in range(0, len(text), 2)]).lstrip("0")
+
+class IntToHexCommand(Base):
     def op(self, text):
         return "#{0:02x}".format(int(text))
