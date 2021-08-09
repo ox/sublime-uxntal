93185a9bf80bc8676d230eb77a2e7b0bc8ff4d35
Artem Titoulenko <artem.titoulenko@gmail.com>
2021-08-09 18:52:58 -0400

    string to hex, hex to string, int to hex commands

--- conversions.py
+++ conversions.py
@@ -7,7 +7,7 @@ class Base(sublime_plugin.TextCommand):
         regions = self.view.sel()
         for region in regions:
             text = self.view.substr(region)
-            self.view.replace(edit, region, self.op(self.sanitize(text)))
+            self.view.replace(edit, region, self.op(text))
 
     def sanitize(self, text):
         return re.sub(r"([\s\#])", "", text.lower())
@@ -17,16 +17,28 @@ class Base(sublime_plugin.TextCommand):
 
 class BinToHexCommand(Base):
     def op(self, text):
-        return "#{0:04x}".format(int(text, 2))
+        return "#{0:04x}".format(int(self.sanitize(text), 2))
 
 class HexToBinCommand(Base):
     def op(self, text):
+        text = self.sanitize(text)
         return " ".join(["{0:08b}".format(int(text[i:i+2], 16)) for i in range(0, len(text), 2)])
 
 class HexToIntCommand(Base):
     def op(self, text):
+        text = self.sanitize(text)
         return "".join([str(int(text[i:i+2], 16)) for i in range(0, len(text), 2)]).lstrip("0")
 
 class IntToHexCommand(Base):
     def op(self, text):
+        text = self.sanitize(text)
         return "#{0:02x}".format(int(text))
+
+class StringToHexCommand(Base):
+    def op(self, text):
+        return " ".join(["{0:02x}".format(ord(text[i:i+1])) for i in range(0, len(text))])
+
+class HexToStringCommand(Base):
+    def op(self, text):
+        text = self.sanitize(text)
+        return "".join([chr(int(text[i:i+2], 16)) for i in range(0, len(text), 2)]).lstrip("0")        
